405f2a9c28c84822024b85a3dc20d39b0887ff2d
Szabri <128305053+Brigihh@users.noreply.github.com>
2025-12-11 11:20:16 +0100

    üres react keretrendszer

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react' //azért nem akítv mert jelenleg nem használjuk
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1>Ez lesz a főoldal.</h1>
+
+      </div>
+      
+    </>
+  )
+}
+
+export default App
